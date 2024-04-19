2856db035c5815dae552ab732b8edd89a52e77b0
artem-python <64275869+artem-python@users.noreply.github.com>
2024-04-19 12:33:07 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,28 @@
+# Simple
+
+a = 1  # int  # In our example "a" is universal var
+a = 1.0  # float
+a = 1 + 1j  # complex number
+a = True  # bool
+a = None  # or null #empty type
+
+# Complex
+
+a = 'ala pinga'  # string or str into C++
+a = [1, 1.6, 'testo']  # list
+a = (3, 7.3, 'world')  # tuple
+a = frozenset({0, 2.5, 'peace'})  # set or frozenset
+a = {'key1': 'value1', 'key2': 'value2'}  # dictionary
+a = b'bts'  # byte type
+a = bytearray(b'some code')  # byte array
+
+# Callable
+
+help(a)  # return some info about a
+print()  # write into python console
+input("Some input data")  # return user input with string as argument
+len(a)  # length of a array or list
+min(a)  # return min value into a array or list
+# or
+max(a)  # return max value into a array or list
+type(a)  # returns type a
