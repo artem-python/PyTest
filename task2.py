a2b36b229021cb9ce2eef1426a96283d3ee533d2
Lucifer <64275869+artem-python@users.noreply.github.com>
2024-04-22 11:34:42 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,19 +1,21 @@
 # Task 1
 
-print("The number of books on the media: %s" % (int(float(input("Enter the storage capacity (in MB): ")) * 1048576) // (int(input("Number of bytes per character: ")) * int(input("Enter the number of characters per line: ")) * int(input("Enter the number of lines per page: ")) * int(input("Enter the number of pages in the book: ")))))
+print("The number of books on the media: %s" % (int(float(input("Enter the storage capacity (in MB): ")) * 1048576) // (
+            int(input("Number of bytes per character: ")) * int(
+        input("Enter the number of characters per line: ")) * int(input("Enter the number of lines per page: ")) * int(
+        input("Enter the number of pages in the book: ")))))
 
 # Task 2
 
-print() #I use this to make space between tasks😜
-match input("Enter calc mode(circle, square): "):
-    case "circle":
-        radius = float(input("Enter the radius(may in float)😁: "))
-        print("Area: %s " % float(3.1415 * radius ** 2.0))
-        print("Length: %s" % float(2.0 * 3.1415 * radius))
-    case "square":
-        side = float(input("Enter the radius(may in float)😉: "))
-        print(f"Perimeter: {float(4 * side)}")
-        print(f"Area: {float(side ** 2)}")
+print()  # I use this to make space between tasks😜
+if input("Enter calc mode(circle, square): ") == "circle":
+    radius = float(input("Enter the radius(may in float)😁: "))
+    print("Area: %s " % float(3.1415 * radius ** 2.0))
+    print("Length: %s" % float(2.0 * 3.1415 * radius))
+elif input("Enter calc mode(circle, square): ") == "square":
+    side = float(input("Enter the radius(may in float)😉: "))
+    print(f"Perimeter: {float(4 * side)}")
+    print(f"Area: {float(side ** 2)}")
 
 # Task 3
 
@@ -23,7 +25,8 @@ print('0' * 20 + '1' * 50 + '2' * 30)
 # Task 4
 
 print()
-paid_data = (500 - (float(input('Enter the speed in kb/s: ')) * float(input("Enter the download time: ")))) * float(input("Introduction the cost of one kilobyte: "))
+paid_data = (500 - (float(input('Enter the speed in kb/s: ')) * float(input("Enter the download time: ")))) * float(
+    input("Introduction the cost of one kilobyte: "))
 print("You need to pay: %s" % paid_data if paid_data < 0 else "No payment is required")
 
 # Task 5 (boring 😩)
