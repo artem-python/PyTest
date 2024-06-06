233fde9ca4e166a6dfe2e5dab1b56b9226d2ec84
Lucifer <64275869+artem-python@users.noreply.github.com>
2024-06-06 11:01:13 +0300

    Add files via upload

---
+++ practice8.py
@@ -0,0 +1,18 @@
+def mincustom(listdata: list = []):
+    return min(listdata)
+
+
+def maxcustom(listdata: list = []):
+    return max(listdata)
+
+
+def sumcustom(listdata: list = []):
+    return sum(listdata)
+
+def multi(number:int = 1):
+    if number == 1:
+        return number == 0
+    elif number == 2:
+        pass
+    else:
+        return number % 2
